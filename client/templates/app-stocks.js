963e091f51b04e604ff90118b8b2b016bd7d9ff1
Xinlai He <Xinlai_He@epam.com>
2016-04-01 13:22:56 +0800

    update initial stocks and figure out best bid and ask

--- client/templates/app-stocks.js
+++ client/templates/app-stocks.js
@@ -6,7 +6,27 @@ Template.appStocks.onCreated(function bodyOnCreated() {
 
 Template.appStocks.helpers({
   stocks() {
-    return stockLists.find({});
+
+    var stocks = stockLists.find({});
+    var stocks_info = [];
+    stocks.forEach(function(stock){
+
+      var bids = stock.BID;
+      var asks = stock.ASK;
+      var bestBid = bids[0].price;
+      var bestAsk = asks[0].price
+      for(var i = 1; i < bids.length; i++){
+        if(bids[i].price > bestBid) bestBid = bids[i].price;
+      }
+      for(var i = 1; i < asks.length; i++){
+        if(asks[i].price < bestAsk) bestAsk = asks[i].price;
+      }
+      stock.bestBid = bestBid.toFixed(2);
+      stock.bestAsk = bestAsk.toFixed(2);
+      stocks_info.push(stock);
+    });
+
+    return stocks_info;
   },
 });
 
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,10 +2,54 @@
 Meteor.startup(function () {
   if (stockLists.find().count() === 0) {
     var data = [
-      {companyName: "Microsoft.Corp", tradingSymbol: "MSFT", ASK: 80.02, BID: 80.00, tradingVolumn: 0},
-      {companyName: "Google.Corp", tradingSymbol: "GOOG", ASK: 30.02, BID: 30.00, tradingVolumn: 0},
-      {companyName: "Epam.Ltd", tradingSymbol: "EPM", ASK: 11.52, BID: 11.50, tradingVolumn: 0},
-      {companyName: "Alibaba.Ltd", tradingSymbol: "ALIB", ASK: 20.02, BID: 20.00, tradingVolumn: 0}
+      {companyName: "Microsoft.Corp", tradingSymbol: "MSFT", ASK:
+        [
+          {price : 80.06, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 80.04, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 80.02, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], BID:
+        [
+          {price : 80.00, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 79.98, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 79.96, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], tradingVolumn: 0
+      },
+      {companyName: "Google.Corp", tradingSymbol: "GOOG", ASK:
+        [
+          {price : 30.06, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 30.04, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 30.02, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], BID:
+        [
+          {price : 30.00, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 29.98, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 29.96, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], tradingVolumn: 0
+      },
+      {companyName: "Epam.Ltd", tradingSymbol: "EPM", ASK:
+        [
+          {price : 11.06, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 11.04, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 11.02, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], BID:
+        [
+          {price : 11.00, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 10.98, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 10.96, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], tradingVolumn: 0
+      },
+      {companyName: "Alibaba.Ltd", tradingSymbol: "ALIB", ASK:
+        [
+          {price : 50.06, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 50.04, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 50.02, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], BID:
+        [
+          {price : 50.00, quantity : 100, trader : "System", time : "2016-04-01 10:10:10"},
+          {price : 49.98, quantity : 200, trader : "System", time : "2016-04-01 10:10:11"},
+          {price : 49.96, quantity : 150, trader : "System", time : "2016-04-01 10:10:12"}
+        ], tradingVolumn: 0
+      }
     ];
 
     _.each(data, function(list) {
